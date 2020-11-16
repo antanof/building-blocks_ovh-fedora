51adb564f710aadc8c43d45e1749bec2738ef524
Fontana <matthieu.fontana@itcloud.ovh>
2020-11-16 23:23:56 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -22,18 +22,3 @@ provider "ovh" {
   endpoint = "ovh-eu"
   alias    = "ovh"
 }
-
-# # Configure le fournisseur OpenStack hébergé par OVH
-# provider "openstack" {
-#  auth_url = "https://auth.cloud.ovh.net/v3.0/" # URL d'authentification
-#  domain_name = "default" # Nom de domaine - Toujours à "default" pour OVH
-#  tenant_name = ""
-#  alias = "ovh"
-#  user_name   = var.openstack_user
-#  password    = var.openstack_password
-# }
-#
-# provider "ovh" {
-#  endpoint = "ovh-eu" # Point d'entrée du fournisseur
-#  alias = "ovh" # Un alias
-# }
